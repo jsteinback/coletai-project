49fda7a6b92997fed135664c4e037332619d205d
Jessica Steinback <je.steinback@gmail.com>
2023-05-12 00:08:46 -0300

    ajuste procfile

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ localStorage = new LocalStorage('./scratch');
 
 const coletaiRoutes = require('./src/routes/routes');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
     res.send('Coletai')
