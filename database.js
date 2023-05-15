52b2f199ea87449c8adb27b27ce7ea938cc3c46c
Jessica Steinback <je.steinback@gmail.com>
2023-05-15 20:57:53 -0300

    alterado banco de dados para heroku pg

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    user: 'ysytufdjvogjso',
+    password: 'd4093ed5e46efca4a0b3a64879e0a191ef4551f1b05d8f65e3a423a5e81c04cf',
     host: 'ec2-34-236-103-63.compute-1.amazonaws.com',
     port: 5432,
     database: 'dfucq07op878vt'
