781b56e1f3e3af2fbaae22e35f151dab21daaf1a
Jessica Steinback <je.steinback@gmail.com>
2023-05-15 21:46:21 -0300

    ajustes nas cofigurações de banco de dados

--- database.js
+++ database.js
@@ -1,11 +1,10 @@
-const Pool = require('pg').Pool;
+const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'ysytufdjvogjso',
-    password: 'd4093ed5e46efca4a0b3a64879e0a191ef4551f1b05d8f65e3a423a5e81c04cf',
-    host: 'ec2-34-236-103-63.compute-1.amazonaws.com',
-    port: 5432,
-    database: 'dfucq07op878vt'
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 module.exports = pool;
